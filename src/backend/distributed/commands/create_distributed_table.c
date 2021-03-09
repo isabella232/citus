ed151f5f03afa02e033c7c846d034d6e67a5fbbd
Halil Ozan Akgul <hozanakgul@gmail.com>
2021-03-09 10:43:43 +0300

    Creates new colocation for colocate_with:='none' too

--- src/backend/distributed/commands/create_distributed_table.c
+++ src/backend/distributed/commands/create_distributed_table.c
@@ -959,7 +959,8 @@ ColocationIdForNewTable(Oid relationId, Var *distributionColumn,
 		}
 		else if (IsColocateWithNone(colocateWithTableName))
 		{
-			colocationId = GetNextColocationId();
+			colocationId = CreateColocationGroup(shardCount, ShardReplicationFactor,
+												 distributionColumnType, distributionColumnCollation);
 
 			createdColocationGroup = true;
 		}
